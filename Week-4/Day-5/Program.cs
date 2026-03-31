256aab3b32fde26c0a5fe7b3ccdb7bc3adb1aac1
DUDEKULA AFREEN <126767558+DUDEKULAAFREEN@users.noreply.github.com>
2026-03-31 16:45:21 +0530

    Add files via upload

---
+++ Week-4/Day-5/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class BankAccount
+{
+    // Private field (Encapsulation)
+    private double balance = 0;
+
+    // Deposit method
+    public void Deposit(double amount)
+    {
+        if (amount > 0)
+        {
+            balance += amount;
+            Console.WriteLine("Deposited: " + amount);
+        }
+        else
+        {
+            Console.WriteLine("Invalid deposit amount!");
+        }
+    }
+
+    // Withdraw method
+    public void Withdraw(double amount)
+    {
+        if (amount <= balance)
+        {
+            balance -= amount;
+            Console.WriteLine("Withdrawn: " + amount);
+        }
+        else
+        {
+            Console.WriteLine("Insufficient balance!");
+        }
+    }
+
+    // Get balance method
+    public double GetBalance()
+    {
+        return balance;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        BankAccount account = new BankAccount();
+
+        // Sample operations
+        account.Deposit(1000);
+        account.Withdraw(300);
+
+        // Display balance
+        Console.WriteLine("Current Balance = " + account.GetBalance());
+    }
+}
