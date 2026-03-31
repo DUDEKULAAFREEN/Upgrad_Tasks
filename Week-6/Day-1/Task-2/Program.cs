b2c517ed2c8aa363963de79985bf2a3fde84c7c0
DUDEKULA AFREEN <126767558+DUDEKULAAFREEN@users.noreply.github.com>
2026-03-31 19:17:20 +0530

    Add files via upload

---
+++ Week-6/Day-1/Task-2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter Product Name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter Product Price: ");
+        double price = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter Discount Percentage: ");
+        double discount = Convert.ToDouble(Console.ReadLine());
+
+        // Correct Formula
+        double finalPrice = price - (price * discount / 100);
+
+        Console.WriteLine("\nProduct Name: " + name);
+        Console.WriteLine("Original Price: " + price);
+        Console.WriteLine("Discount: " + discount + "%");
+        Console.WriteLine("Final Price: " + finalPrice);
+
+        Console.ReadLine();
+    }
+}
