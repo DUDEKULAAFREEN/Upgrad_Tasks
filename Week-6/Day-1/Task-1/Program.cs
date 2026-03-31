6af5e5886b3afd3f1f7aa1a5f2f45d7813219f46
DUDEKULA AFREEN <126767558+DUDEKULAAFREEN@users.noreply.github.com>
2026-03-31 19:11:57 +0530

    Add files via upload

---
+++ Week-6/Day-1/Task-1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task WriteLogAsync(string message)
+    {
+        Console.WriteLine($"Start writing: {message}");
+
+        await Task.Delay(2000);
+
+        Console.WriteLine($"Completed writing: {message}");
+    }
+
+    static async Task Main()
+    {
+        Console.WriteLine("Logging started...\n");
+
+        Task t1 = WriteLogAsync("Log Entry 1");
+        Task t2 = WriteLogAsync("Log Entry 2");
+        Task t3 = WriteLogAsync("Log Entry 3");
+
+        Console.WriteLine("Main thread is still running...\n");
+
+        await Task.WhenAll(t1, t2, t3);
+
+        Console.WriteLine("\nAll logs written successfully!");
+        Console.ReadLine();
+    }
+}
