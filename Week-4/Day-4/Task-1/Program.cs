538a827ade9049cd6ed5f896e9c33f2a92c421b4
DUDEKULA AFREEN <126767558+DUDEKULAAFREEN@users.noreply.github.com>
2026-03-31 16:13:24 +0530

    Add files via upload

---
+++ Week-4/Day-4/Task-1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+// Create Calculator class
+class Calculator
+{
+    // Method for Addition
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    // Method for Subtraction
+    public int Subtract(int a, int b)
+    {
+        return a - b;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Taking input
+        Console.Write("Enter First Number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter Second Number: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        // Creating object of Calculator
+        Calculator calc = new Calculator();
+
+        // Calling methods
+        int addition = calc.Add(num1, num2);
+        int subtraction = calc.Subtract(num1, num2);
+
+        // Display output
+        Console.WriteLine("Addition = " + addition);
+        Console.WriteLine("Subtraction = " + subtraction);
+    }
+}
