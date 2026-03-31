b1f7cac37dc9920038aaac28903a58a0d5f04e96
DUDEKULA AFREEN <126767558+DUDEKULAAFREEN@users.noreply.github.com>
2026-03-31 15:48:04 +0530

    Add files via upload

---
+++ Week-4/Day-3/Task-4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Input
+        Console.Write("Enter Number: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int evenCount = 0;
+        int oddCount = 0;
+        int sum = 0;
+
+        // Loop from 1 to N
+        for (int i = 1; i <= n; i++)
+        {
+            sum += i;
+
+            if (i % 2 == 0)
+                evenCount++;
+            else
+                oddCount++;
+        }
+
+        // Output
+        Console.WriteLine("Even Count: " + evenCount);
+        Console.WriteLine("Odd Count: " + oddCount);
+        Console.WriteLine("Sum: " + sum);
+    }
+}
