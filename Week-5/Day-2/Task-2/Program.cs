51e8a5fb281002fb197d9b7552bafbc3ae305a56
DUDEKULA AFREEN <126767558+DUDEKULAAFREEN@users.noreply.github.com>
2026-03-31 18:03:52 +0530

    Add files via upload

---
+++ Week-5/Day-2/Task-2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Calculator
+{
+    public void Divide(int numerator, int denominator)
+    {
+        try
+        {
+            int result = numerator / denominator;
+            Console.WriteLine("Result = " + result);
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Error: Cannot divide by zero");
+        }
+        finally
+        {
+            Console.WriteLine("Operation completed safely");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter Numerator: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter Denominator: ");
+        int den = Convert.ToInt32(Console.ReadLine());
+
+        Calculator calc = new Calculator();
+
+        // Calling method
+        calc.Divide(num, den);
+
+        // Program continues
+        Console.WriteLine("Program continues after handling exception...");
+    }
+}
