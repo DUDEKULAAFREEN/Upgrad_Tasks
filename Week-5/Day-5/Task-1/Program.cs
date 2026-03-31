01ab3566fb7d67aa045527f61a4ecb751b915d0a
DUDEKULA AFREEN <126767558+DUDEKULAAFREEN@users.noreply.github.com>
2026-03-31 18:42:07 +0530

    Add files via upload

---
+++ Week-5/Day-5/Task-1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Enter a message: ");
+            string message = Console.ReadLine();
+
+            // Open file in Append mode
+            using (FileStream fs = new FileStream("log.txt", FileMode.Append, FileAccess.Write))
+            {
+                byte[] data = Encoding.UTF8.GetBytes(message + Environment.NewLine);
+                fs.Write(data, 0, data.Length);
+            }
+
+            Console.WriteLine("Message written successfully!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+
+        Console.ReadLine();
+    }
+}
